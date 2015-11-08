2f4f04feb916862fc6f2c7206fe088f140343c7d
Russell Benton <portlight.rschlup@gmail.com>
2015-11-08 12:43:36 -0500

    Allows for an array of methods i.e. [ ‘get’, ‘post’, ‘all’ ] and adds req.api to the request so the user know what controller and action was called. Very useful for creating dynamic views, or app.use

--- index.js
+++ index.js
@@ -1,28 +1,46 @@
 var ctrlr = require('ctrlr');
 
+
+var allowedMethods = [ 'get', 'post', 'put', 'delete', 'patch', 'all' ];
 module.exports = function(expressApp, config) {
-    if (typeof config.routes !== 'object') {
-        throw new Error('config.routes must be set to a valid route object map');
-    }
+	if (typeof config.routes !== 'object') {
+		throw new Error('config.routes must be set to a valid route object map');
+	}
 
-    if (typeof config.controllers !== 'string') {
-        throw new Error('config.controllers must be set to a valid directory path');
-    }
+	if (typeof config.controllers !== 'string') {
+		throw new Error('config.controllers must be set to a valid directory path');
+	}
 
-    var controllers = ctrlr(config.controllers);
+	var controllers = ctrlr(config.controllers);
 
-    for (var route in config.routes) {
-        var meta = config.routes[route];
+	function assignRoute(route, method, meta) {
+		var method = (method || 'all').toLowerCase();
+		if(allowedMethods.indexOf(method) === -1) throw new Error('Method: '+method+' - is not a valide method type');
 
-        if (typeof meta === 'string') {
-            meta = { action: meta };
-        }
+		expressApp[method](route, function (req, res, next) {
+			var action = meta.action.split('#');
+			req.api = {
+				'route': route,
+				'controller': action[0],
+				'action': action[1],
+			};
+			return next();
+		});
+		expressApp[method](route, controllers(meta.action));
+	}
 
-        var method = (meta.method || 'get').toLowerCase();
-        var action = controllers(meta.action);
+	for (var route in config.routes) {
+		var meta = config.routes[route];
 
-        expressApp[method](route, action);
-    }
+		if (typeof meta === 'string') {
+			meta = { action: meta };
+		}
+		if (Array.isArray(meta.method)) {
+			for( var m in meta.method){
+				assignRoute(route, meta.method[m], meta);
+			}
+		}else{
+			assignRoute(route, meta.method, meta);
+		}
+	}
 };
-
-
